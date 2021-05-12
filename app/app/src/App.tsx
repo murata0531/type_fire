540ed4152ba3d6d37743655b50ef155a5230cac9
murata0531 <coless115@gmail.com>
2021-05-12 20:11:01 +0900

    firestore

--- app/app/src/App.tsx
+++ app/app/src/App.tsx
@@ -2,6 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+(async () => {
+  try {
+    const firebase = require('firebase')
+    const config = {
+      apiKey: '### FIREBASE API KEY ###',
+      authDomain: '### FIREBASE AUTH DOMAIN ###',
+      projectId: '### CLOUD FIRESTORE PROJECT ID ###',
+    }
+    firebase.initializeApp(config)
+    const db = firebase.firestore()
+
+    const userRef = db.collection('users').doc('kpUVKKold2s0rMcDnPTx')
+    const userDoc = await userRef.get()
+    if (userDoc.exists) {
+      console.log(userDoc.id)
+      console.log(userDoc.data())
+      console.log(userDoc.get('name'))
+      console.log(userDoc.get('old'))
+    } else {
+      console.log('No such document!')
+    }
+    await db.app.delete()
+  } catch (err) {
+    console.log(`Error: ${JSON.stringify(err)}`)
+  }
+})()
+
 function App() {
   return (
     <div className="App">
@@ -16,7 +43,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactkkkk
+          Learn React
         </a>
       </header>
     </div>
