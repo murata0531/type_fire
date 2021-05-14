b9e8c5d8f81cd8295b2a1ab8f9aa1b2abe26753f
murata0531 <coless115@gmail.com>
2021-05-15 02:21:05 +0900

    a

--- app/app/functions/src/index.ts
+++ app/app/functions/src/index.ts
@@ -1,38 +1,28 @@
-import * as functions from "firebase-functions";
+const functions = require('firebase-functions')
+// cloud functionでfirestoreを使うのに必要な設定は以下の２行
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 
-// The Firebase Admin SDK to access Firestore.
-const admin = require("firebase-admin");
-admin.initializeApp();
+// データベースの参照を作成
+var fireStore = admin.firestore()
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+exports.helloWorld = functions.https.onRequest((request: any, response: { send: (arg0: string) => void }) => {
+  // 動作確認のため適当なデータをデータベースに保存
+  var citiesRef = fireStore.collection('cities');
+  citiesRef.doc('SF').set({
+    name: 'San Francisco', state: 'CA', country: 'USA',
+    capital: false, population: 860000 })
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection("messages").add({original: original});
-    // Send back a message that we've successfully written the message
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
-    });
-
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore.document("/messages/{documentId}")
-    .onCreate((snap, context) => {
-        // Grab the current value of what was written to Firestore.
-        const original = snap.data().original;
-
-        // Access the parameter `{documentId}` with `context.params`
-        functions.logger.log("Uppercasing", context.params.documentId, original);
-
-        const uppercase = original.toUpperCase();
-
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to Firestore.
-        // Setting an 'uppercase' field in Firestore document returns a Promise.
-        return snap.ref.set({uppercase}, {merge: true});
-    });
+  var cityRef = fireStore.collection('cities').doc('SF')
+  cityRef.get()
+  .then((doc: { exists: any; data: () => any }) => {
+    if (!doc.exists) {
+      response.send('No such document!')
+    } else {
+      response.send(doc.data())
+      }
+    })
+    .catch((err: any) => {
+      response.send('not found')
+    })
+})
