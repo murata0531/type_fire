7be4f8c21d56a3f29d5cf9dced64975921eefb53
murata0531 <coless115@gmail.com>
2021-05-14 20:43:34 +0900

    a

--- app/app/functions/src/index.ts
+++ app/app/functions/src/index.ts
@@ -1,22 +1,5 @@
-// import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
 
-import { Url } from "url";
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-export const helloWorld = functions.https.onRequest((request:typeof functions, response:typeof functions) => {
-    response.send("Hello from Firebase!");
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
 });
